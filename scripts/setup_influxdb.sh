18c7a199cc048cdd1f7a36a4d770c0f211b998a1
Frédéric Pinaud <fpinaud17@gmail.com>
2020-09-01 11:29:46 +0200

    Resolved typo in setup_influxdb.sh

--- scripts/setup_influxdb.sh
+++ scripts/setup_influxdb.sh
@@ -5,6 +5,6 @@ docker run -d --name influxdb -p 9999:9999 quay.io/influxdb/influxdb:2.0.0-beta
 while ! $(curl -sS 'http://localhost:9999/ready' | grep -q ready); do echo 'Waiting for influx...'; sleep 1; done
 
 echo "2) exporting env var for terraform acceptance tests"
-export $INFLUXDB_V2_URL="http://localhost:9999"
-export $INFLUXDB_V2_USERNAME="test_acc_user"
-export $INFLUXDB_V2_PASSWORD="test_acc_pass"
+export INFLUXDB_V2_URL="http://localhost:9999"
+export INFLUXDB_V2_USERNAME="test_acc_user"
+export INFLUXDB_V2_PASSWORD="test_acc_pass"
